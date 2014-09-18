6832479fa07de89435123e2464011571b795ae4a
David Branner <databranner@gmail.com>
2014-09-18 17:17:39 -0400

    update scrape and presentation

--- scrape.py
+++ scrape.py
@@ -3,9 +3,48 @@
 # David Prager Branner
 # 20140918
 
-def hexify(title)"
+import urllib
+import io
+import lxml.etree
+parser = lxml.etree.HTMLParser()
+
+def hexify(title):
     """Generate a hex string from kanji string, with percent prefixing."""
     # Timeit shows encode() to have same running time as bytes(str, 'utf-8')
-    return ('%' + 
+    return ('%' +
             '%'.join([hex(item)[2:].upper() for item in list(title.encode())]
-           )
+           ))
+
+def compose_api_req(title):
+    return ('''http://zh.wikipedia.org/w/api.php?''' +
+          '''action=query&''' +
+          '''generator=allpages&''' +
+          '''prop=info&''' +
+          '''format=xml&''' +
+          '''titles=''' + hexify(title))
+
+def fetch_page_api(title):
+    api = compose_api_req(title)
+    page = urllib.request.urlopen(api).read()
+    return page
+
+def fetch_page_html(title):
+    api = 'https://zh.wikipedia.org/wiki/' + hexify(title)
+    page = urllib.request.urlopen(api).read()
+    return page
+
+def get_words(title):
+    page = fetch_page_html(title)
+    root = lxml.etree.parse(io.StringIO(page.decode()), parser)
+    # Note: Obama page returns empty but shouldn't - too long?
+    codes = root.xpath('//div[@data-noteta-code]')
+    ds = []
+    if codes:
+        for code in [code.values()[-1].split(';') for code in codes]:
+            d = {}
+            for pair in code:
+                if pair:
+                    k, v = pair.strip(' ').split(':')
+                    d[k] = v
+            ds.append(d)
+    return ds
