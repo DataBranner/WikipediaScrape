9753d6c418f5af16f1ba306da19d238cb5c340a1
David Branner <databranner@gmail.com>
2014-10-03 09:57:07 -0400

    revise crawl: finally, no dups; condense/flush I/O

--- code/crawl.py
+++ code/crawl.py
@@ -14,79 +14,79 @@ import sys
 import json
 
 def main():
-    break_loop = False
     start_time = time.time()
-    while not break_loop:
-        # Get the collection of links. 
-        unscraped_links_filename = os.path.join(
-                '..', 'data', 'links', 'links_unscraped.txt')
-        with open(unscraped_links_filename, 'r') as f:
-            links = f.read()
-        links = links.split('\n')
-        print('Retrieved {} unscraped links from {}'.
-                format(len(links), unscraped_links_filename))
-        # If empty, collect newest links (ignore other matter). 
-        #     http://en.wikipedia.org/wiki/Special:RecentChanges
-        if links == ['']:
-            print('No links found in file\n    {}'.
-                    format(unscraped_links_filename))
-            _, _, _, links = S.main('Special:RecentChanges')
-            print('Retrieved {} links from "Special:RecentChanges".'.
-                    format(len(links)))
-        # If these have been done already, get random link.
-        #     https://zh.wikipedia.org/wiki/Special:Random
-        if not links:
-            links = ['Special:Random']
-            print('Turning to "Special:Random".')
-        links = set(links)
-        if '' in links:
-            links.remove('')
-        # Also get the list of done links.
-        done_links_filename = os.path.join(
-                '..', 'data', 'links', 'done_links.txt')
-        with open(done_links_filename, 'r') as f:
-            done_links = f.read()
-        done_links = set(done_links.split('\n'))
-        if done_links == {''}:
-            done_links = set()
-        print('Retrieved {} done links from {}'.
-                format(len(done_links), done_links_filename))
-        while links:
-            try:
-                links, done_links = scrape_links(
-                        links, done_links, unscraped_links_filename,
-                        done_links_filename, start_time)
-            except KeyboardInterrupt:
-                print('''\nWe had KeyboardInterrupt; links: |{}|. '''.
-                        format(len(links)))
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                traceback.print_exception(exc_type, exc_value, exc_traceback)
-                break_loop = True
-                break
-            except Exception:
-                print('\nWe had Exception; links: |{}|.'.
-                        format(len(links)))
-                exc_type, exc_value, exc_traceback = sys.exc_info()
-                traceback.print_exception(exc_type, exc_value, exc_traceback)
-                break_loop = True
-                break
+    # Get the collection of links. 
+    unscraped_links_filename = os.path.join(
+            '..', 'data', 'links', 'links_unscraped.txt')
+    with open(unscraped_links_filename, 'r') as f:
+        links = f.read()
+    links = links.split('\n')
+    print('Retrieved {} unscraped links from {}'.
+            format(len(links), unscraped_links_filename))
+    # If empty, collect newest links (ignore other matter). 
+    #     http://en.wikipedia.org/wiki/Special:RecentChanges
+    if links == ['']:
+        print('No links found in file\n    {}'.
+                format(unscraped_links_filename))
+        _, _, _, links = S.main('Special:RecentChanges')
+        print('Retrieved {} links from "Special:RecentChanges".'.
+                format(len(links)))
+    # If these have been done already, get random link.
+    #     https://zh.wikipedia.org/wiki/Special:Random
+    if not links:
+        links = ['Special:Random']
+        print('Turning to "Special:Random".')
+    links = set(links)
+    if '' in links:
+        links.remove('')
+    # Also get the list of done links.
+    done_links_filename = os.path.join(
+            '..', 'data', 'links', 'done_links.txt')
+    with open(done_links_filename, 'r') as f:
+        done_links = f.read()
+    done_links = set(done_links.split('\n'))
+    if done_links == {''}:
+        done_links = set()
+    print('Retrieved {} done links from {}'.
+            format(len(done_links), done_links_filename))
+    try:
+        links, done_links = scrape_links(
+                links, done_links, unscraped_links_filename,
+                done_links_filename, start_time)
+    except KeyboardInterrupt:
+        print('''\nWe had KeyboardInterrupt; links: |{}|. '''.
+                format(len(links)))
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+    except Exception:
+        print('\nWe had Exception; links: |{}|.'.
+                format(len(links)))
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
 
 def scrape_links(
-        links, done_links, unscraped_links_filename, done_links_filename, 
-        start_time):
-    with open(unscraped_links_filename, 'w') as f, open(done_links_filename, 'w') as g:
+    links, done_links, unscraped_links_filename, done_links_filename, 
+    start_time):
+    syn_count = len(os.listdir(os.path.join('..', 'data', 'synonyms_new')))
+    with open(unscraped_links_filename, 'a') as f, open(done_links_filename, 'a') as g:
         while links:
+            old_link_length = len(links)
             title = links.pop()
+            if len(links) == old_link_length:
+                print('len(links) == old_link_length: {}'.format(len(links)))
+                sys.exit()
             # Ignore if title already done.
             if title in done_links:
                 continue
             page, title, synonyms, new_links = S.main(title)
+            syn_count += len(synonyms)
             if title in new_links:
                 new_links.remove(title)
+            new_links = new_links - links
             links.update(new_links)
-            print('Time: {}; Links left: {}; synonyms: {}; new links: {}; {}'.
-                    format(int(time.time() - start_time), len(links), 
-                           len(synonyms), len(new_links), title))
+            print('T: {}; links: +{}=>{}; syn.: +{}=>{}; {}'.
+                    format(int(time.time() - start_time), len(new_links), 
+                           len(links), len(synonyms), syn_count, title))
             # Uncomment the following line to save whole pages (compressed).
             # _ = U.store_data(page, title, target_dir='html_new', tar=True)
             if synonyms:
@@ -95,9 +95,11 @@ def scrape_links(
                         target_dir='synonyms_new', tar=False)
             # Update done_links.
             done_links.add(title)
-            f.write('\n'.join(links))
-            g.write('\n'.join(done_links))
-        time.sleep(2)
+            f.write('\n'.join(new_links))
+            f.flush()
+            g.write('\n' + title)
+            g.flush()
+            time.sleep(2)
     return links, done_links
 
 if __name__ == '__main__':
