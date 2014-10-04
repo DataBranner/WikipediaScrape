bab18d2d1db85d9be56172ac5fbd191e729e025e
David Branner <databranner@gmail.com>
2014-10-04 14:39:47 -0400

    add large number of new data files

--- code/crawl.py
+++ code/crawl.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # crawl.py
 # David Prager Branner
-# 20141002, works
+# 20141004, works
 
 """"""
 import utils as U
@@ -14,10 +14,17 @@ import sys
 import json
 
 def main():
-    start_time = time.time()
+    while True:
+        try:
+            scrape_links(title=None)
+        except KeyboardInterrupt:
+            print('''\nWe had KeyboardInterrupt in main(). ''')
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback.print_exception(exc_type, exc_value, exc_traceback)
+            break
+
+def get_unscraped_links(unscraped_links_filename):
     # Get the collection of links. 
-    unscraped_links_filename = os.path.join(
-            '..', 'data', 'links', 'links_unscraped.txt')
     with open(unscraped_links_filename, 'r') as f:
         links = f.read()
     links = links.split('\n')
@@ -39,9 +46,9 @@ def main():
     links = set(links)
     if '' in links:
         links.remove('')
-    # Also get the list of done links.
-    done_links_filename = os.path.join(
-            '..', 'data', 'links', 'done_links.txt')
+    return links
+
+def get_done_links(done_links_filename):
     with open(done_links_filename, 'r') as f:
         done_links = f.read()
     done_links = set(done_links.split('\n'))
@@ -49,67 +56,71 @@ def main():
         done_links = set()
     print('Retrieved {} done links from {}'.
             format(len(done_links), done_links_filename))
-    while True:
-        try:
-            links, done_links = scrape_links(
-                    links, done_links, unscraped_links_filename,
-                    done_links_filename, start_time)
-        except KeyboardInterrupt:
-            print('''\nWe had KeyboardInterrupt; links: |{}|. '''.
-                    format(len(links)))
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            traceback.print_exception(exc_type, exc_value, exc_traceback)
-            break
+    return done_links
 
-def scrape_links(
-    links, done_links, unscraped_links_filename, done_links_filename, 
-    start_time):
+def update_links(links, new_links, done_links, title):
+    new_links -= set(title)
+    new_links = new_links.difference(links).difference(done_links)
+    links.update(new_links)
+    # Update done_links.
+    done_links.add(title)
+    return links, new_links, done_links
+
+def scrape_links(title=None,
+        unscraped_links_filename=os.path.join(
+            '..', 'data', 'links', 'links_unscraped.txt'), 
+        done_links_filename=os.path.join(
+            '..', 'data', 'links', 'done_links.txt')):
+    start_time = time.time()
+    links = get_unscraped_links(unscraped_links_filename)
+    done_links = get_done_links(done_links_filename)
     syn_count = len(os.listdir(os.path.join('..', 'data', 'synonyms_new')))
     print('Found {} synonym-files at start of while-loop.\n'.format(syn_count))
     with open(unscraped_links_filename, 'a') as f, open(done_links_filename, 'a') as g:
         while links:
-            old_link_length = len(links)
             title = links.pop()
-            if len(links) == old_link_length:
-                print('len(links) == old_link_length: {}'.format(len(links)))
-                sys.exit()
             # Ignore if title already done.
             if title in done_links:
-                links -= set(title)
                 continue
             try:
                 page, title, synonyms, new_links = S.main(title)
             except KeyboardInterrupt:
-                print('''\nWe met with KeyboardInterrupt; links: |{}|. '''.
-                        format(len(links)))
+                print('''\nWe met with KeyboardInterrupt; title: {}. '''.
+                        format(title))
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 traceback.print_exception(exc_type, exc_value, exc_traceback)
                 break
+            except TypeError:
+                # TypeError: 'NoneType' object is not iterable
+                # Usually because "HTTP Error 404: Not Found", so restore title.
+                # But many of these are corrupt; do not restore for now.
+#                links.add(title)
+                print('    {}'.format(title))
+                continue
             except Exception:
-                print('\nWe met with Exception; links: |{}|.'.
-                        format(len(links)))
+                print('\nWe met with Exception; title: {}.'.
+                        format(title))
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 traceback.print_exception(exc_type, exc_value, exc_traceback)
                 print('\n')
-            syn_count += len(synonyms)
-            new_links = new_links - links - set(title)
-            links.update(new_links)
+                continue
+            if synonyms:
+                _ = U.store_data(
+                        json.dumps(synonyms).encode(), title, 
+                        target_dir='synonyms_new', tar=False)
+                syn_count += len(synonyms)
+            links, new_links, done_links = update_links(
+                    links, new_links, done_links, title)
             print('T: {}; links: + {:>4} => {:>}; syn.: + {} => {}; {}'.
                     format(int(time.time() - start_time), len(new_links), 
                            len(links), len(synonyms), syn_count, title))
             # Uncomment the following line to save whole pages (compressed).
             # _ = U.store_data(page, title, target_dir='html_new', tar=True)
-            if synonyms:
-                _ = U.store_data(
-                        json.dumps(synonyms).encode(), title, 
-                        target_dir='synonyms_new', tar=False)
-            # Update done_links.
-            done_links.add(title)
-            f.write('\n'.join(new_links))
+            f.write('n' + '\n'.join(new_links))
             f.flush()
             g.write('\n' + title)
             g.flush()
-            time.sleep(2)
+            time.sleep(1.2)
     return links, done_links
 
 if __name__ == '__main__':
